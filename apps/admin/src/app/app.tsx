5a4032ce25d409c04ec90d7f489395f81ec6352f
longstupay <761798834@qq.com>
2022-12-02 12:11:41 +0800

    feat(admin): set up tailwind for admin app

---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,55 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+import { Route, Routes, Link } from 'react-router-dom';
+import { TopicButton } from '@longstupay/taib-ui';
+
+export function App() {
+  return (
+    <>
+      {/* <NxWelcome title="administrator" /> */}
+      <TopicButton topicName="Next.js" />
+      <div />
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link className='text-blue-600' to="/">Home</Link>
+          </li>
+          <li>
+            <Link className='text-red-600' to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
+    </>
+  );
+}
+
+export default App;
---
+++ apps/admin/tailwind.config.js
@@ -0,0 +1,20 @@
+const { join } = require('path');
+
+// available since Nx v 12.5
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
